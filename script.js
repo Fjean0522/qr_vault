cce03430dabe0f2ea20d9e32178597b95c35dc34
Fjean <fjean0522@gmail.com>
2023-09-09 13:17:25 -0400

    Create function get form details.
    If empty an alert is sent to add url.
    
    Loading spinner displays while loading.

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('generate-form');
+const qr = document.getElementById('qrcode');
+
+const submitDetails = (e) => {
+    e.preventDefault();
+
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if (url === '') {
+        alert('Enter a URL')
+    } else {
+        loadSpinner();
+        setTimeout(() => {
+            hideSpinner();
+        }, 1000);
+    };
+};
+
+const loadSpinner = () => {
+    document.getElementById('load-spinner').style.display = 'block'
+}
+
+const hideSpinner = () => {
+    document.getElementById('load-spinner').style.display = 'none'
+}
+
+form.addEventListener('submit', submitDetails);
