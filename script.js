fb0ab1c7d6b43da9bb486a131d55a27f093b9e1d
Fjean <fjean0522@gmail.com>
2023-09-09 14:05:17 -0400

    Create functions to clear ui and download qr code.
    ClearUI function removes previous qrcode from page.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const qrcode = document.getElementById('qrcode');
 const submitDetails = (e) => {
     e.preventDefault();
 
+    clearUI();
+
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
 
@@ -13,8 +15,15 @@ const submitDetails = (e) => {
         loadSpinner();
 
         setTimeout(() => {
+
             hideSpinner();
             generateQRCode(url, size);
+
+            setTimeout(() => {
+              const saveUrl = qrcode.querySelector('img').src;
+              downloadBtn(saveUrl);
+            }, 50);
+
         }, 1000);
     };
 };
@@ -29,10 +38,29 @@ const generateQRCode = (url, size) => {
 
 const loadSpinner = () => {
     document.getElementById('load-spinner').style.display = 'block'
-}
+};
 
 const hideSpinner = () => {
     document.getElementById('load-spinner').style.display = 'none'
-}
+};
+
+const clearUI = () => {
+    qrcode.innerHTML = '';
+    const saveLink = document.getElementById('save-link');
+
+    if (saveLink) {
+        saveLink.remove();
+    }
+};
+
+const downloadBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.href = saveUrl;
+    link.download = 'qrcode';
+    link.innerHTML = 'Download QR Code';
+    document.getElementById('generated').appendChild(link);
+};
 
 form.addEventListener('submit', submitDetails);
